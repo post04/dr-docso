8dd8114bbe2f479a71ed704d4e987684113883b3
post <61803796+postrequest69@users.noreply.github.com>
2021-03-05 12:52:38 -0500

    change command names

--- main.go
+++ main.go
@@ -46,8 +46,8 @@ func main() {
 
 	cmdhandler := New(c.Prefix, true)
 	cmdhandler.AddCommand("docs", "{prefix}docs github.com/bwmarrin/discordgo", "Get the documentation of a package from pkg.go.dev", cmd.HandleDoc)
-	cmdhandler.AddCommand("getfuncs", "{prefix}getfuncs github.com/bwmarrin/discordgo", "Get all the functions in a package from pkg.go.dev", cmd.HandleFuncsPages)
-	cmdhandler.AddCommand("gettypes", "{prefix}gettypes github.com/bwmarrin/discordgo", "Get all the types in a package from pkg.go.dev", cmd.HandleTypesPages)
+	cmdhandler.AddCommand("funcs", "{prefix}funcs github.com/bwmarrin/discordgo", "Get all the functions in a package from pkg.go.dev", cmd.HandleFuncsPages)
+	cmdhandler.AddCommand("types", "{prefix}types github.com/bwmarrin/discordgo", "Get all the types in a package from pkg.go.dev", cmd.HandleTypesPages)
 	cmdhandler.GenHelp()
 	bot.AddHandler(cmdhandler.OnMessage)
 	err = bot.Open()
